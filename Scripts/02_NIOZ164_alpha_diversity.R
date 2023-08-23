315fa09715de79174c3787f8b5f6f2ce3f94b7ec
Maaike Goudriaan <maaike.goudriaan@nioz.nl>
2023-08-23 16:17:51 +0200

    alphadiv analysis

---
+++ Scripts/02_NIOZ164_alpha_diversity.R
@@ -0,0 +1,337 @@
+##%#####################################################################################%##
+# NIOZ164 Statia Discs - Alpha Diversity                                              #####                                            
+# Author: Maaike Goudriaan, NIOZ, MMB                                                     #
+#                                                                                         #
+# Purpose:                                                                                # 
+# NIOZ164 is an illumina sequencing lane consisting of different samplesets.              # 
+# Here we focus on analysis of 16S amplicon data, from amplified DNA                      #
+# extracted from discs covered in polymer films that were incubated at 2 different        #
+# locations in 2 different waterdepths (seafloor and watercolumn) in                      #
+# coastal Caribbean waters close to the island of St. EUstatius.                          #
+# The tested polymers were PE, PP, PS, PEt and Nylon, PE-13C and PP-13C.                  #
+# Of each polymere there was an UV pretreated and a non-treated version.                  #
+##%#####################################################################################%##
+
+# Date: 2023 - 08 - 23
+# R-version: 4.3.1 
+
+
+# Set working directory ------------------------------------------------------------------
+setwd("C:/Users/mgoudriaan/Documents/GitHub/Caribbean_discs_longterm/Scripts")
+
+# Load libraries -------------------------------------------------------------------------
+library(devtools)
+library(phyloseq)
+library(grid)
+library(tidyverse)
+library(vegan)
+library(compositions)
+library(ggforce)
+library(ggdist)
+library(gghalves)
+library(ggrepel)
+library(microbiome)
+library("remotes")
+library("ggh4x")
+library(ggpubr)
+library("ggsci")
+library("cowplot")
+library(rvg)
+library(officer)
+
+# Import data ----------------------------------------------------------------------------
+physeq_object <- readRDS("../Analysis/NIOZ164_physeq_object.rds")
+
+# Colors for plotting --------------------------------------------------------------------
+pal_isme <- c("#006d77", "#ffddd2", "#00C49A", "#e29578", "#83c5be")
+Pal.plast <- c("#DDCC77","#117733", "#AA4499", "#88CCEE", "#332288" )
+pal.time <- c("#44AA99", "#882255")
+pal.uv <- c("#999933", "#CC6677")
+
+colors_M1 <- c("#004e64", "#ecc8af", "#F2AF29", "#436436", "#00a5cf", 
+               "#c18c5d", "#5f0f40", "#DC602E", "#495867", "#A29F15", 
+               "#570000", "#FFF5B2", "#20221B", "#9fffcb", "#c08497", 
+               "#8D6346", "#FF4B3E", "#149911", "#472d30")
+
+# Calculate alpha diversity indices -----------------------------------------------------
+Alpha.Div <- phyloseq::estimate_richness(physeq_object, measures=c("Observed", "Simpson", "Shannon", "Chao1") )
+head(Alpha.Div)
+
+# Add metadata to the dataframe for plotting
+Alpha.Discs   <- Alpha.Div %>% cbind(data.frame(physeq_object@sam_data)) 
+
+# add new factors combined column
+Alpha.Discs$Method_Phase <- str_c(Alpha.Discs$Method, "_", Alpha.Discs$Phase)
+Alpha.Discs$Backbone_Isotope <- str_c(Alpha.Discs$Backbone, "_", Alpha.Discs$Isotope)
+
+#remove unecessary columns and rows
+Alpha.Discs$InputFileName <- NULL
+#We want to focus only on the discs themselfs and the collected polymers for the alpha diversity
+Alpha.Discs <- Alpha.Discs %>%  filter( !Polymer %in% c("B", "NA") & Habitat != "Unknown")
+unique(Alpha.Discs$Polymer)
+unique(Alpha.Discs$Habitat)
+unique(Alpha.Discs$Method)
+
+head(Alpha.Discs)
+
+# Make exploratory raincloud plots to store in pptx---------------------------------------------------
+## Chao1 -----------------------------------------------------------------------------------
+Chao1.Discs <- read_pptx()
+Chao1.Discs  <- read_pptx("../Reports/Chao1.NIOZ164.pptx")
+
+Chao1 <- ggplot(Alpha.Discs,          #Pick data to plot
+                  aes(x = Treatment, y = Chao1,  fill = Treatment)) + #Pick factors to use
+  geom_boxplot(aes(alpha = 0.5), # Make boxplot
+               width = 0.1, outlier.colour = NULL) +
+  geom_half_violin(aes(alpha = 0.5),side = "r", nudge = 0.15, scale = "count") + # Make half-violin
+  geom_half_point(aes(color = Treatment),                                       # Chose factor for points
+                  side = 'l', position = position_nudge(x=-0.1), range_scale = 0.3) +
+  theme_pubclean()+
+  theme(axis.text.x=element_text(size = 12, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 12), 
+        legend.text=element_text(size = 12),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text.x = element_text(size = 13),
+        plot.title = element_blank(),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank()) +
+  guides(alpha = "none") +
+ facet_grid( ~ Location) +
+  scale_colour_manual(values = colors_M1) +
+  scale_fill_manual(values = colors_M1) 
+
+Chao1
+
+editable_graph <- dml(ggobj = Chao1)
+Chao1.Discs  <- add_slide(Chao1.Discs ) 
+Chao1.Discs <- ph_with(x = Chao1.Discs, editable_graph,location = ph_location_type(type = "body") )
+print(Chao1.Discs , target = "../Reports/Chao1.NIOZ164.pptx")
+
+## Observed Features -----------------------------------------------------------------------------------
+Observed.Discs <- read_pptx()
+Observed.Discs  <- read_pptx("../Reports/Observed.Features.NIOZ164.pptx")
+
+Obs.feat <- ggplot(Alpha.Discs,          #Pick data to plot
+                aes(x = Treatment, y = Observed,  fill = Treatment)) + #Pick factors to use
+  geom_boxplot(aes(alpha = 0.5), # Make boxplot
+               width = 0.1, outlier.colour = NULL) +
+  geom_half_violin(aes(alpha = 0.5),side = "r", nudge = 0.15, scale = "count") + # Make half-violin
+  geom_half_point(aes(color = Treatment),                                       # Chose factor for points
+                  side = 'l', position = position_nudge(x=-0.1), range_scale = 0.3) +
+  theme_pubclean()+
+  theme(axis.text.x=element_text(size = 12, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 12), 
+        legend.text=element_text(size = 12),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text.x = element_text(size = 13),
+        plot.title = element_blank(),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank()) +
+  guides(alpha = "none") +
+  facet_grid( ~ Location) +
+  scale_colour_manual(values = colors_M1) +
+  scale_fill_manual(values = colors_M1) 
+
+Obs.feat
+
+editable_graph <- dml(ggobj = Obs.feat)
+Observed.Discs  <- add_slide(Observed.Discs ) 
+Observed.Discs <- ph_with(x = Observed.Discs, editable_graph,location = ph_location_type(type = "body") )
+print(Chao1.Discs , target = "../Reports/Observed.Features.NIOZ164.pptx")
+
+## Simpson -----------------------------------------------------------------------------------
+Simpson.Discs <- read_pptx()
+Simpson.Discs  <- read_pptx("../Reports/Simpson.NIOZ164.pptx")
+
+Simpson <- ggplot(Alpha.Discs,          #Pick data to plot
+                   aes(x = Treatment, y = Simpson,  fill = Treatment)) + #Pick factors to use
+  geom_boxplot(aes(alpha = 0.5), # Make boxplot
+               width = 0.1, outlier.colour = NULL) +
+  geom_half_violin(aes(alpha = 0.5),side = "r", nudge = 0.15, scale = "count") + # Make half-violin
+  geom_half_point(aes(color = Treatment),                                       # Chose factor for points
+                  side = 'l', position = position_nudge(x=-0.1), range_scale = 0.3) +
+  theme_pubclean()+
+  theme(axis.text.x=element_text(size = 12, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 12), 
+        legend.text=element_text(size = 12),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text.x = element_text(size = 13),
+        plot.title = element_blank(),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank()) +
+  guides(alpha = "none") +
+  facet_grid( ~ Location) +
+  scale_colour_manual(values = colors_M1) +
+  scale_fill_manual(values = colors_M1) 
+
+Simpson
+
+editable_graph <- dml(ggobj = Simpson)
+Simpson.Discs  <- add_slide(Simpson.Discs ) 
+Simpson.Discs<- ph_with(x = Simpson.Discs, editable_graph,location = ph_location_type(type = "body") )
+print(Chao1.Discs , target = "../Reports/Simpson.NIOZ164.pptx")
+
+## Shannon -----------------------------------------------------------------------------------
+Shannon.Discs <- read_pptx()
+Shannon.Discs  <- read_pptx("../Reports/Shannon.NIOZ164.pptx")
+
+Shannon <- ggplot(Alpha.Discs,          #Pick data to plot
+                  aes(x = Treatment, y = Shannon,  fill = Treatment)) + #Pick factors to use
+  geom_boxplot(aes(alpha = 0.5), # Make boxplot
+               width = 0.1, outlier.colour = NULL) +
+  geom_half_violin(aes(alpha = 0.5),side = "r", nudge = 0.15, scale = "count") + # Make half-violin
+  geom_half_point(aes(color = Treatment),                                       # Chose factor for points
+                  side = 'l', position = position_nudge(x=-0.1), range_scale = 0.3) +
+  theme_pubclean()+
+  theme(axis.text.x=element_text(size = 12, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 12), 
+        legend.text=element_text(size = 12),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.x = element_text(size=15),
+        axis.title.y = element_text(size=15),
+        strip.text.x = element_text(size = 13),
+        plot.title = element_blank(),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank()) +
+  guides(alpha = "none") +
+  facet_grid( ~ Location) +
+  scale_colour_manual(values = colors_M1) +
+  scale_fill_manual(values = colors_M1) 
+
+Shannon
+
+editable_graph <- dml(ggobj = Shannon)
+Shannon.Discs  <- add_slide(Shannon.Discs ) 
+Shannon.Discs<- ph_with(x = Shannon.Discs, editable_graph,location = ph_location_type(type = "body") )
+print(Chao1.Discs , target = "../Reports/Shannon.NIOZ164.pptx")
+
+
+# Make final point - errobar plots ------------------------------------------------------
+
+## Summarize alpha diversity indices in average and SD ----------------------------------
+# First pito dataframe into long format
+Alpha.Discs.long <- Alpha.Discs  %>% select(!se.chao1) %>%  pivot_longer(cols = c("Observed", "Chao1", "Shannon", "Simpson"), names_to = "Diversity_Index", 
+                                                                         values_to = "Value")
+head(Alpha.Discs.long)
+#load needed formula
+source("summarySE.R")
+
+# Summarize and calculate avg and SD
+# Define groups per which you want to have the mean and SD and you want to keep for plotting
+Alpha.Discs.long.c <-  summarySE(Alpha.Discs.long, measurevar="Value", groupvars=c("Location", "Habitat","Polymer", "Isotope", "Backbone", "Treatment", 
+                                                                                   "Location_Habitat", "Diversity_Index"))
+# Select richness and diversity seperately for plotting
+Richness.long.c <- Alpha.Discs.long.c %>% filter(Diversity_Index %in% c("Observed", "Chao1"))
+Diversity.long.c <- Alpha.Discs.long.c %>% filter(Diversity_Index %in% c("Shannon", "Simpson"))
+
+## Observed/Chao1?? --------------------------------------------------------------------
+Chao1 <- ggplot(chao1.c,         #Pick data to plot
+                aes(x=interaction(Material, category), y = Chao1, fill = Material, color = Material)) + #Pick factors to use
+  geom_errorbar(aes(ymin=Chao1-se, ymax=Chao1+se, width=.3)) +
+  geom_point(size = 4) +
+  facet_nested( ~ timepoint + treatment) +
+  guides( x = "axis_nested") +
+  theme_pubclean()+
+  theme(legend.position = "top",
+        legend.key = element_rect(fill = "white", colour = "white"),
+        axis.text.x=element_text(size = 15, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 15), 
+        legend.text=element_text(size = 15),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.y = element_text(size= 20),
+        strip.text.x = element_text(size = 17),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank(),
+        ggh4x.axis.nestline.x = element_line(linetype = c(6,1), linewidth = 1, color = c("black", "darkgrey")),
+        ggh4x.axis.nesttext.x = element_text(angle = 0, color = c("black", "darkgrey"), hjust = 0.5)) +
+  scale_colour_manual(values = Pal.plast) +
+  scale_fill_manual(values = Pal.plast) +
+  xlab("") +
+  labs( title = "", color = "Polymers", fill = "Polymers")
+
+Chao1
+
+## Simpson --------------------------------------------------------------------
+Simpson <- ggplot(Simpson.c,         #Pick data to plot
+                  aes(x=interaction(Material, category), y = Simpson, fill = Material, color = Material)) + #Pick factors to use
+  geom_errorbar(aes(ymin=Simpson-se, ymax=Simpson+se, width=.3)) +
+  geom_point(size = 4) +
+  facet_nested( ~ timepoint + treatment) +
+  guides( x = "axis_nested") +
+  theme_pubclean()+
+  theme(legend.position = "top",
+        legend.key = element_rect(fill = "white", colour = "white"),
+        axis.text.x=element_text(size = 15, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 15), 
+        legend.text=element_text(size = 15),
+        legend.title = element_text(size=15, face = "bold"),
+        axis.title.y = element_text(size= 20),
+        strip.text.x = element_text(size = 17),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank(),
+        ggh4x.axis.nestline.x = element_line(linetype = c(6,1), linewidth = 1, color = c("black", "darkgrey")),
+        ggh4x.axis.nesttext.x = element_text(angle = 0, color = c("black", "darkgrey"), hjust = 0.5)) +
+  scale_colour_manual(values = Pal.plast) +
+  scale_fill_manual(values = Pal.plast) +
+  xlab("") +
+  ylab("Gini-Simpson Index") +
+  labs( title = "", color = "Polymers", fill = "Polymers")
+
+## Shannon --------------------------------------------------------------------
+
+Shannon <- ggplot(Shannon.c,         #Pick data to plot
+                  aes(x=interaction(Material, category), y = Shannon, fill = Material, color = Material)) + #Pick factors to use
+  geom_errorbar(aes(ymin=Shannon-se, ymax=Shannon+se, width=.3)) +
+  geom_point(size = 4) +
+  facet_nested( ~ timepoint + treatment) +
+  guides( x = "axis_nested") +
+  theme_pubclean()+
+  theme(legend.position = "bottom",
+        legend.key = element_rect(fill = "white", colour = "white"),
+        axis.text.x=element_text(size = 15, angle = 60, hjust = 1), 
+        axis.text.y=element_text(size= 15), 
+        legend.text=element_text(size = 15),
+        legend.title = element_text(size=18),
+        axis.title.y = element_text(size= 20),
+        strip.text.x = element_text(size = 17),
+        panel.border = element_rect(color = "grey90", fill = NA),
+        panel.grid.major.y = element_line(color = "grey90", linetype = 3),
+        panel.grid.major.x = element_blank(),
+        ggh4x.axis.nestline.x = element_line(linetype = c(6,1), linewidth = 1, color = c("black", "darkgrey")),
+        ggh4x.axis.nesttext.x = element_text(angle = 0, color = c("black", "darkgrey"), hjust = 0.5)) +
+  scale_colour_manual(values = Pal.plast) +
+  scale_fill_manual(values = Pal.plast) +
+  xlab("") +
+  ylab("Shannon index") +
+  labs( title = "", color = "Polymers", fill = "Polymers")
+
+Shannon
+
+## Combine panels with cowplot 
+legend.a <- get_legend(Shannon+
+                         theme(legend.direction = "horizontal",
+                               legend.title.align = 0.5))
+
+
+
+plot_grid(Chao1 + theme(legend.position ="none"),
+          Simpson + theme(legend.position ="none"),
+          Shannon, 
+          ncol = 2,
+          nrow = 2,
+          align = 'v',
+          axis = "l",
+          rel_heights = c(1,1.1),
+          rel_widths = c(1,1))
