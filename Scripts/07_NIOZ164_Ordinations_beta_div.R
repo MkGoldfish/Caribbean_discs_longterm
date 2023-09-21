84af6fc2f6d9915eda23791c8e86b542e067d5de
Maaike Goudriaan <maaike.goudriaan@nioz.nl>
2023-09-21 11:48:22 +0200

    statistics per habitat done

--- Scripts/07_NIOZ164_Ordinations_beta_div.R
+++ Scripts/07_NIOZ164_Ordinations_beta_div.R
@@ -15,7 +15,6 @@
 # Date: 2023 - 08 - 23
 # R-version: 4.3.1 
 
-
 ## Set working directory ------------------------------------------------------------------
 setwd("C:/Users/mgoudriaan/Documents/GitHub/Caribbean_discs_longterm/Scripts")
 set.seed(42)
@@ -49,6 +48,10 @@ pal.habs.i<- c("#FF8E32FF", "#51C3CCFF")
 # Beach, Benthic, Pelagic
 pal.loc.hab <- c("#FF6DB6FF","#FFB6DBFF", "#004949FF", "#009292FF", "#66A61E")
 # CB_P, CB_B, CC_P, CB_B, Zeelandia
+pal.CB <- c("#FF6DB6FF", "#FFB6DBFF")
+pal.CC <- c("#004949FF", "#009292FF")
+pal.pel <- c("#FF6DB6FF", "#004949FF")
+pal.ben <- c("#FFB6DBFF", "#009292FF")
 
 pal.pols.isotop <- c("#E31A1C", "#7570B3", "#1F78B4","#A6CEE3", "#E6AB02","#A6761D", "#E5C494","#1B9E77","#882255")
 pal.pols <- c("#E31A1C", "#7570B3" ,"#1F78B4","#E6AB02", "#A6761D", "#1B9E77","#882255")
@@ -151,6 +154,24 @@ samp_i.cb <- as.data.frame(as.matrix(sample_data(physeq_inc_CB)))
 # "Highest taxa sum is 83017"
 # Number of singletons = 380"
 
+### 5.Pelagic Only----
+physeq_inc_P <- subset_samples(physeq_inc, Habitat == "Pelagic")
+physeq_inc_P <- prune_taxa(taxa_sums(physeq_inc_P)>0, physeq_inc_P)
+summarize_phyloseq(physeq_inc_P)
+basic_info_physeq_object(physeq_inc_P)
+otu_p<- as.data.frame(otu_table(physeq_inc_P)) %>% t()
+samp_p <- as.data.frame(as.matrix(sample_data(physeq_inc_P)))
+
+
+### 6. Benthic Only   ----
+physeq_inc_B <- subset_samples(physeq_inc, Habitat == "Benthic")
+physeq_inc_B <- prune_taxa(taxa_sums(physeq_inc_B)>0, physeq_inc_B)
+summarize_phyloseq(physeq_inc_B)
+basic_info_physeq_object(physeq_inc_B)
+otu_b<- as.data.frame(otu_table(physeq_inc_B)) %>% t()
+samp_b <- as.data.frame(as.matrix(sample_data(physeq_inc_B)))
+
+
 ## CLR transformation of ASV tables -----
 # Check that samples are rows and ASVs are columns
 rownames(otu_i)
@@ -161,18 +182,24 @@ otu_i.w.no0 <- cmultRepl(otu_i.w, method = "CZM", output = "p-counts")
 otu_i.no0 <- cmultRepl(otu_i, method = "CZM", output = "p-counts")
 otu_i.cc.no0 <- cmultRepl(otu_i.cc, method = "CZM", output = "p-counts")
 otu_i.cb.no0 <- cmultRepl(otu_i.cb, method = "CZM", output = "p-counts")
+otu_p.no0 <- cmultRepl(otu_p, method = "CZM", output = "p-counts")
+otu_b.no0 <- cmultRepl(otu_b, method = "CZM", output = "p-counts")
 
 # perform clr 
 clr_i.w <- as.data.frame(clr(otu_i.w.no0))
 clr_i <- as.data.frame(clr(otu_i.no0))
 clr_i_CC <- as.data.frame(clr(otu_i.cc.no0))
 clr_i_CB <- as.data.frame(clr(otu_i.cb.no0))
+clr_i_P <- as.data.frame(clr(otu_p.no0))
+clr_i_B <- as.data.frame(clr(otu_b.no0))
 
 # Check that clr_data and metadata rownames match
 summary(rownames(samp_i.w) == rownames(clr_i.w))
 summary(rownames(samp_i) == rownames(clr_i))
 summary(rownames(samp_i.cc) == rownames(clr_i_CC))
 summary(rownames(samp_i.cb) == rownames(clr_i_CB))
+summary(rownames(samp_p) == rownames(clr_i_P))
+summary(rownames(samp_b) == rownames(clr_i_B))
 
 
 # 1. Calculate distances for 3 locations --------------------------------------------------------------
@@ -316,7 +343,7 @@ pcoa_cc <- wcmdscale(aitd_i_cc, eig = T)
 # Lets plot the relative eigenvalues of all axes
 barplot(as.vector(pcoa_cc$eig)/sum(pcoa_cc$eig))
 
-pct_xplnd <- pcoa_i.w$eig/sum(pcoa_i.w$eig) *100
+pct_xplnd <- pcoa_cc$eig/sum(pcoa_cc$eig) *100
 pretty_px <- format(round(pct_xplnd[1:2], digits = 1), nsmall = 1, trim = TRUE)
 labs <- c(glue("PCo1 ({pretty_px[1]}%)"),
           glue("PCo2 ({pretty_px[2]}%)"))
@@ -382,7 +409,7 @@ pcoa_cb <- wcmdscale(aitd_i_cb, eig = T)
 # Lets plot the relative eigenvalues of all axes
 barplot(as.vector(pcoa_cb$eig)/sum(pcoa_cb$eig))
 
-pct_xplnd <- pcoa_i.w$eig/sum(pcoa_i.w$eig) *100
+pct_xplnd <- pcoa_cb$eig/sum(pcoa_cb$eig) *100
 pretty_px <- format(round(pct_xplnd[1:2], digits = 1), nsmall = 1, trim = TRUE)
 labs <- c(glue("PCo1 ({pretty_px[1]}%)"),
           glue("PCo2 ({pretty_px[2]}%)"))
@@ -440,15 +467,159 @@ PCoA_plot_CB <-
 
 PCoA_plot_CB
 
-# Grid of both incubation locations w cowpolt ---------------
+# Grid of both incubation locations w cowplot ---------------
 legend.a <- get_legend(PCoA_plot_CC)
 
 plot_grid(PCoA_plot_CB + theme(legend.position ="none"),
-          PCoA_plot_CC + theme(legend.position ="none"),
           legend.a,
+          PCoA_plot_CC + theme(legend.position ="none"),
           ncol = 3,
+          nrow = 1,
+          labels = c('A', '', 'B'),
+          align = 'v',
+          axis = "l",
+          rel_widths = c(1,0.2,1))
+
+# 5. Calculate distances for Pelagic only --------------------------------------------------------------
+# Aitchison distance it the Euclidean distance of (r)clr transformed data
+aitd_P <- vegdist(clr_i_P, method = "euclidean")
+
+pcoa_P <- wcmdscale(aitd_P, eig = T)
+# Lets plot the relative eigenvalues of all axes
+barplot(as.vector(pcoa_P$eig)/sum(pcoa_P$eig))
+
+pct_xplnd <- pcoa_P$eig/sum(pcoa_P$eig) *100
+pretty_px <- format(round(pct_xplnd[1:2], digits = 1), nsmall = 1, trim = TRUE)
+labs <- c(glue("PCo1 ({pretty_px[1]}%)"),
+          glue("PCo2 ({pretty_px[2]}%)"))
+
+#How much do the first 2 axes explain in total?
+sum((as.vector(pcoa_P$eig)/sum(pcoa_P$eig))[1:2]) * 100
+# ~22%
+
+#and the first 3
+sum((as.vector(pcoa_P$eig)/sum(pcoa_P$eig))[1:3]) * 100
+# ~30.1%
+
+## Plotting with GGPLOT ------------------------------------------------
+data.scores <- as.data.frame(pcoa_P$points)[1:2] 
+colnames(data.scores) <- c("PCo1", "PCo2")
+dim(data.scores)
+
+data.scores$Description = samp_p$Description
+data.scores$Location= samp_p$Location
+data.scores$Location_Habitat = samp_p$Location_Habitat
+data.scores$Polymer = samp_p$Polymer
+data.scores$Polymer_Isotope = samp_p$Polymer_Isotope
+data.scores$Habitat= samp_p$Habitat
+data.scores$Backbone = samp_p$Backbone
+data.scores$Treatment = samp_p$Treatment
+
+head(data.scores)
+
+shapes <- c(15,17,18,19,4,8,13)
+
+## Create NMDS ordination plot
+PCoA_plot_P <- 
+  # Create axis based on both NMDS values in 2 dimensions
+  ggplot(data.scores, aes(x = PCo1, y = PCo2)) +  
+  #Plot the points of the NMDS, select what you want as shape and color from metadata
+  geom_point(size = 5, aes(color = Location, shape = Polymer, stroke = 1))  +
+  theme_pubr() +
+  theme(axis.text.y = element_text(colour = "black", size = 10),
+        axis.text.x = element_text(colour = "black", size = 10),
+        legend.text = element_text(size = 10, colour ="black"),
+        legend.position="bottom", legend.box = "vertical", 
+        axis.title.y = element_text(face = "bold", size = 11),
+        axis.title.x = element_text(face = "bold", size = 11, colour = "black"),
+        legend.title = element_text(size = 11, colour = "black", face = "bold"),
+        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, linewidth = 1.2),
+        legend.key=element_blank(),
+        plot.title = element_text(size = 12, face = "bold", hjust = 0.5, vjust = 1)) +
+  # Set axislabels and title 
+  labs(x = labs[1], y = labs[2])  + 
+  scale_shape_manual(values = shapes) + #<- Specify shapes you want as vector. 
+  scale_fill_manual(values = pal.pel) +      #<- Pick the correct colorpalette, based on your fill 
+  scale_color_manual(values = pal.pel) +     #<- Pick the correct colorpalette, based on your fill 
+  guides(fill = guide_legend(override.aes = list( shape = 22, linetype = NULL)),  #<- Costumize the legend. All color swatches are now squares, to avoid confusion with the other shapes
+         shape = guide_legend(override.aes = list(fill = "black", colopr = "black")))  #<- Make ure shapes are same as the ones you picked, and are all filled and black
+
+PCoA_plot_P
+
+# 6. Calculate distances for Benthic only --------------------------------------------------------------
+# Aitchison distance it the Euclidean distance of (r)clr transformed data
+aitd_B <- vegdist(clr_i_B, method = "euclidean")
+
+pcoa_B <- wcmdscale(aitd_B, eig = T)
+# Lets plot the relative eigenvalues of all axes
+barplot(as.vector(pcoa_B$eig)/sum(pcoa_B$eig))
+
+pct_xplnd <- pcoa_B$eig/sum(pcoa_B$eig) *100
+pretty_px <- format(round(pct_xplnd[1:2], digits = 1), nsmall = 1, trim = TRUE)
+labs <- c(glue("PCo1 ({pretty_px[1]}%)"),
+          glue("PCo2 ({pretty_px[2]}%)"))
+
+#How much do the first 2 axes explain in total?
+sum((as.vector(pcoa_B$eig)/sum(pcoa_B$eig))[1:2]) * 100
+# ~19.2%
+
+#and the first 3
+sum((as.vector(pcoa_B$eig)/sum(pcoa_B$eig))[1:3]) * 100
+# ~25.8%
+
+## Plotting with GGPLOT ------------------------------------------------
+data.scores <- as.data.frame(pcoa_B$points)[1:2] 
+colnames(data.scores) <- c("PCo1", "PCo2")
+dim(data.scores)
+
+data.scores$Description = samp_b$Description
+data.scores$Location= samp_b$Location
+data.scores$Location_Habitat = samp_b$Location_Habitat
+data.scores$Polymer = samp_b$Polymer
+data.scores$Polymer_Isotope = samp_b$Polymer_Isotope
+data.scores$Habitat= samp_b$Habitat
+data.scores$Backbone = samp_b$Backbone
+data.scores$Treatment = samp_b$Treatment
+
+head(data.scores)
+
+shapes <- c(15,17,18,19,4,8,13)
+
+## Create NMDS ordination plot
+PCoA_plot_B <- 
+  # Create axis based on both NMDS values in 2 dimensions
+  ggplot(data.scores, aes(x = PCo1, y = PCo2)) +  
+  #Plot the points of the NMDS, select what you want as shape and color from metadata
+  geom_point(size = 5, aes(color = Location, shape = Polymer, stroke = 1))  +
+  theme_pubr() +
+  theme(axis.text.y = element_text(colour = "black", size = 10),
+        axis.text.x = element_text(colour = "black", size = 10),
+        legend.text = element_text(size = 10, colour ="black"),
+        legend.position="bottom", legend.box = "vertical", 
+        axis.title.y = element_text(face = "bold", size = 11),
+        axis.title.x = element_text(face = "bold", size = 11, colour = "black"),
+        legend.title = element_text(size = 11, colour = "black", face = "bold"),
+        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, linewidth = 1.2),
+        legend.key=element_blank(),
+        plot.title = element_text(size = 12, face = "bold", hjust = 0.5, vjust = 1)) +
+  # Set axislabels and title 
+  labs(x = labs[1], y = labs[2])  + 
+  scale_shape_manual(values = shapes) + #<- Specify shapes you want as vector. 
+  scale_fill_manual(values = pal.ben) +      #<- Pick the correct colorpalette, based on your fill 
+  scale_color_manual(values = pal.ben) +     #<- Pick the correct colorpalette, based on your fill 
+  guides(fill = guide_legend(override.aes = list( shape = 22, linetype = NULL)),  #<- Costumize the legend. All color swatches are now squares, to avoid confusion with the other shapes
+         shape = guide_legend(override.aes = list(fill = "black", colopr = "black")))  #<- Make ure shapes are same as the ones you picked, and are all filled and black
+
+PCoA_plot_B
+
+# Grid of both incubation locations w cowpolt ---------------
+legend.a <- get_legend(PCoA_plot_CC)
+
+plot_grid(PCoA_plot_P,
+          PCoA_plot_B,
+          ncol = 2,
           nrow = 1,
           labels = c('A', 'B'),
           align = 'v',
-          axis = "l",
-          rel_widths = c(1,1,0.2))
+          axis = "tb",
+          rel_widths = c(1,1))
--- Scripts/08_NIOZ164_Permanova_Permdisp.R
+++ Scripts/08_NIOZ164_Permanova_Permdisp.R
@@ -1,6 +1,6 @@
 ##%#####################################################################################%##
-#NIOZ164 Statia Discs - Ordinations                                                  #####                                            
-#Author: Maaike Goudriaan, NIOZ, MMB                                                     #
+#NIOZ164 Statia Discs - PERMANOVA & PERMDISP                                          #####                                            
+#Author: Maaike Goudriaan, NIOZ, MMB                                                      #
 #                                                                                         #
 # Purpose:                                                                                # 
 # NIOZ164 is an illumina sequencing lane consisting of different samplesets.              # 
@@ -10,6 +10,8 @@
 # coastal Caribbean waters close to the island of St. EUstatius.                          #
 # The tested polymers were PE, PP, PS, PEt and Nylon, PE-13C and PP-13C.                  #
 # Of each polymere there was an UV pretreated and a non-treated version.                  #
+#                                                                                         #
+# Test which factor causes significant community differences                              #
 ##%#####################################################################################%##
 
 # Date: 2023 - 08 - 23
@@ -35,26 +37,6 @@ library(microbiome)
 library(glue)
 library(tidyverse)
 
-
-# # Colors for plotting --------------------------------------------------------------------
-# pal_isme <- c("#006d77", "#ffddd2", "#00C49A", "#e29578", "#83c5be")
-# 
-# pal.loc <- c("#FF6DB6FF" , "#004949FF",  "#66A61E")
-# # CB, CC, Zeelandia"
-# pal.habs<- c("#66A61E","#FF8E32FF", "#51C3CCFF")
-# pal.habs.i<- c("#FF8E32FF", "#51C3CCFF")
-# # Beach, Benthic, Pelagic
-# pal.loc.hab <- c("#FF6DB6FF","#FFB6DBFF", "#004949FF", "#009292FF", "#66A61E")
-# # CB_P, CB_B, CC_P, CB_B, Zeelandia
-# 
-# pal.pols.isotop <- c("#E31A1C", "#7570B3", "#1F78B4","#A6CEE3", "#E6AB02","#A6761D", "#E5C494","#1B9E77","#882255")
-# pal.pols <- c("#E31A1C", "#7570B3" ,"#1F78B4","#E6AB02", "#A6761D", "#1B9E77","#882255")
-# # PE;PE-13C;PP;PP-13C;PS;PET;Nylon;Blanco
-# pal.uv <- c("#332288","#DDCC77", "grey") 
-# # UV, noUV, NA
-# pal.uv.2 <- c("#332288","#DDCC77") 
-
- 
 # Import data ----------------------------------------------------------------------------
 source("basic_info_physeq_object.R")
 # Import earlier created physeq object
@@ -148,6 +130,23 @@ samp_i.cb <- as.data.frame(as.matrix(sample_data(physeq_inc_CB)))
 # "Highest taxa sum is 83017"
 # Number of singletons = 380"
 
+### 5.Pelagic Only----
+physeq_inc_P <- subset_samples(physeq_inc, Habitat == "Pelagic")
+physeq_inc_P <- prune_taxa(taxa_sums(physeq_inc_P)>0, physeq_inc_P)
+summarize_phyloseq(physeq_inc_P)
+basic_info_physeq_object(physeq_inc_P)
+otu_p<- as.data.frame(otu_table(physeq_inc_P)) %>% t()
+samp_p <- as.data.frame(as.matrix(sample_data(physeq_inc_P)))
+
+
+### 6. Benthic Only   ----
+physeq_inc_B <- subset_samples(physeq_inc, Habitat == "Benthic")
+physeq_inc_B <- prune_taxa(taxa_sums(physeq_inc_B)>0, physeq_inc_B)
+summarize_phyloseq(physeq_inc_B)
+basic_info_physeq_object(physeq_inc_B)
+otu_b<- as.data.frame(otu_table(physeq_inc_B)) %>% t()
+samp_b <- as.data.frame(as.matrix(sample_data(physeq_inc_B)))
+
 ## CLR transformation of ASV tables -----
 # Check that samples are rows and ASVs are columns
 rownames(otu_i)
@@ -158,18 +157,24 @@ otu_i.w.no0 <- cmultRepl(otu_i.w, method = "CZM", output = "p-counts")
 otu_i.no0 <- cmultRepl(otu_i, method = "CZM", output = "p-counts")
 otu_i.cc.no0 <- cmultRepl(otu_i.cc, method = "CZM", output = "p-counts")
 otu_i.cb.no0 <- cmultRepl(otu_i.cb, method = "CZM", output = "p-counts")
+otu_p.no0 <- cmultRepl(otu_p, method = "CZM", output = "p-counts")
+otu_b.no0 <- cmultRepl(otu_b, method = "CZM", output = "p-counts")
 
 # perform clr 
 clr_i.w <- as.data.frame(clr(otu_i.w.no0))
 clr_i <- as.data.frame(clr(otu_i.no0))
 clr_i_CC <- as.data.frame(clr(otu_i.cc.no0))
 clr_i_CB <- as.data.frame(clr(otu_i.cb.no0))
+clr_i_P <- as.data.frame(clr(otu_p.no0))
+clr_i_B <- as.data.frame(clr(otu_b.no0))
 
 # Check that clr_data and metadata rownames match
 summary(rownames(samp_i.w) == rownames(clr_i.w))
 summary(rownames(samp_i) == rownames(clr_i))
 summary(rownames(samp_i.cc) == rownames(clr_i_CC))
 summary(rownames(samp_i.cb) == rownames(clr_i_CB))
+summary(rownames(samp_p) == rownames(clr_i_P))
+summary(rownames(samp_b) == rownames(clr_i_B))
 
 
 # 1. Calculate distances for 3 locations --------------------------------------------------------------
@@ -199,6 +204,36 @@ write.table(Permanova_i.w.1.q,"../Analysis/Permanova/PERM_inc.wild_additive.txt"
 # Permanova_i.w.3 <- adonis2(aitd_i.w ~ Habitat * Location * (Backbone + Treatment), data = samp_i.w, add = T, na.rm = T, permutations = perm )
 # Permanova_i.w.3 
 
+## PERMDISP i.w. -------------------------------------------------------
+# homogeneity of dispersion test between distances
+Disp_i.w_hab <- betadisper(aitd_i.w, samp_i.w$Habitat)
+Disp_i.w_loc <- betadisper(aitd_i.w, samp_i.w$Location)
+Disp_i.w_loc.hab <- betadisper(aitd_i.w, samp_i.w$Location_Habitat)
+Disp_i.w_bb <- betadisper(aitd_i.w, samp_i.w$Backbone)
+Disp_i.w_treatm <- betadisper(aitd_i.w, samp_i.w$Treatment)
+
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_i.w_hab <- permutest(betadisper(aitd_i.w, samp_i.w$Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                        model = 'full', pairwise = F, permutations = perm)
+PD_i.w_loc <- permutest(betadisper(aitd_i.w, samp_i.w$Location, type = 'median', sqrt.dist = F, add = F ), 
+                        model = 'full', pairwise = F, permutations = perm)
+PD_i.w_loc.hab <- permutest(betadisper(aitd_i.w, samp_i.w$Location_Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                        model = 'full', pairwise = F, permutations = perm)
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_i.w_hab <- p.adjust(PD_i.w_hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i.w_hab.q <- bind_cols(PD_i.w_hab$tab,q_i.w_hab$qvalue, .name_repair = "minimal") 
+write.table(PD_i.w_hab.q,"../Analysis/Permdisp/PD_inc.Wild_hab.txt" )
+
+q_i.w_loc <- p.adjust(PD_i.w_loc$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i.w_loc.q <- bind_cols(PD_i.w_loc$tab,q_i.w_loc$qvalue, .name_repair = "minimal") 
+write.table(PD_i.w_loc.q,"../Analysis/Permdisp/PD_inc.Wild_loc.txt")
+
+q_i.w_loc.hab <- p.adjust(PD_i.w_loc.hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i.w_loc.hab.q <- bind_cols(PD_i.w_loc.hab$tab,q_i.w_loc.hab$qvalue, .name_repair = "minimal") 
+write.table(PD_i.w_loc.hab.q,"../Analysis/Permdisp/PD_inc.Wild_loc.hab.txt" )
+
+
 # 2. Calculate distances for 2 locations, incubations only --------------------------------------------------------------
 # Aitchison distance it the Euclidean distance of (r)clr transformed data
 aitd_i <- vegdist(clr_i, method = "euclidean")
@@ -222,6 +257,34 @@ q_i_p2 <- p.adjust(Permanova_i.2$`Pr(>F)`, method = "BH") %>% enframe(value = "q
 Permanova_i.2.q <- bind_cols(Permanova_i.2,q_i_p2$qvalue, .name_repair = "minimal")
 
 write.table(Permanova_i.2.q,"../Analysis/Permanova/PERM_inc_additive.txt" )
+
+## PERMDISP i. -------------------------------------------------------
+# homogeneity of dispersion test between distances
+Disp_i_hab <- betadisper(aitd_i, samp_i$Habitat)
+Disp_i_loc <- betadisper(aitd_i, samp_i$Location)
+Disp_i_loc.hab <- betadisper(aitd_i, samp_i$Location_Habitat)
+
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_i_hab <- permutest(betadisper(aitd_i, samp_i$Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                        model = 'full', pairwise = F, permutations = perm)
+PD_i_loc <- permutest(betadisper(aitd_i, samp_i$Location, type = 'median', sqrt.dist = F, add = F ), 
+                        model = 'full', pairwise = F, permutations = perm)
+PD_i_loc.hab <- permutest(betadisper(aitd_i, samp_i$Location_Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                            model = 'full', pairwise = F, permutations = perm)
+
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_i_hab <- p.adjust(PD_i_hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i_hab.q <- bind_cols(PD_i_hab$tab,q_i_hab$qvalue, .name_repair = "minimal") 
+write.table(PD_i_hab.q,"../Analysis/Permdisp/PD_inc_hab.txt" )
+
+q_i_loc <- p.adjust(PD_i_loc$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i_loc.q <- bind_cols(PD_i_loc$tab,q_i_loc$qvalue, .name_repair = "minimal") 
+write.table(PD_i_loc.q,"../Analysis/Permdisp/PD_inc_loc.txt")
+
+q_i_loc.hab <- p.adjust(PD_i_loc.hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_i_loc.hab.q <- bind_cols(PD_i_loc.hab$tab,q_i_loc.hab$qvalue, .name_repair = "minimal") 
+write.table(PD_i_loc.hab.q,"../Analysis/Permdisp/PD_inc.Wild_loc.hab.txt" )
 
 
 # 3. Calculate distances for CC only --------------------------------------------------------------
@@ -247,6 +310,18 @@ q_cc_p2 <- p.adjust(Permanova_cc.2$`Pr(>F)`, method = "BH") %>% enframe(value = 
 Permanova_cc.2.q <- bind_cols(Permanova_cc.2,q_cc_p2$qvalue, .name_repair = "minimal")
 
 write.table(Permanova_cc.2.q,"../Analysis/Permanova/PERM_CC_additive.txt" )
+
+
+## PERMDISP CC -------------------------------------------------------
+# homogeneity of dispersion test between distances
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_cc_hab <- permutest(betadisper(aitd_i_cc, samp_i.cc$Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                      model = 'full', pairwise = F, permutations = perm)
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_cc_hab <- p.adjust(PD_cc_hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_cc_hab.q <- bind_cols(PD_cc_hab$tab,q_cc_hab$qvalue, .name_repair = "minimal") 
+write.table(PD_cc_hab.q,"../Analysis/Permdisp/PD_CC_hab.txt" )
 
 
 # 4. Calculate distances for CB only --------------------------------------------------------------
@@ -275,3 +350,86 @@ q_cb_p2 <- p.adjust(Permanova_cb.2$`Pr(>F)`, method = "BH") %>% enframe(value = 
 Permanova_cb.2.q <- bind_cols(Permanova_cb.2,q_cb_p2$qvalue, .name_repair = "minimal")
 
 write.table(Permanova_cb.2.q,"../Analysis/Permanova/PERM_CB_additive.txt" )
+
+## PERMDISP CB -------------------------------------------------------
+# homogeneity of dispersion test between distances
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_cb_hab <- permutest(betadisper(aitd_i_cb, samp_i.cb$Habitat, type = 'median', sqrt.dist = F, add = F ), 
+                       model = 'full', pairwise = F, permutations = perm)
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_cb_hab <- p.adjust(PD_cb_hab$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_cb_hab.q <- bind_cols(PD_cb_hab$tab,q_cb_hab$qvalue, .name_repair = "minimal") 
+write.table(PD_cb_hab.q,"../Analysis/Permdisp/PD_CB_hab.txt" )
+
+
+# 3. Calculate distances for Pelagic only --------------------------------------------------------------
+# Aitchison distance it the Euclidean distance of (r)clr transformed data
+aitd_P<- vegdist(clr_i_P, method = "euclidean")
+
+## PERMANOVA Pel -------------------------------------------------------
+Permanova_p.1<- adonis2(aitd_P ~ Location * Backbone * Treatment, data = samp_p, add = T, na.rm = T, permutations = perm )
+Permanova_p.1
+
+# Permanova_p.2 <- adonis2(aitd_P ~ Location +  Backbone + Treatment, data = samp_p, add = T, na.rm = T, permutations = perm )
+# Permanova_p.2 
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_p_p1 <- p.adjust(Permanova_p.1$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+Permanova_p.1.q <- bind_cols(Permanova_p.1,q_p_p1$qvalue, .name_repair = "minimal") 
+
+write.table(Permanova_p.1.q,"../Analysis/Permanova/PERM_pelagic.full.txt" )
+
+
+# q_cc_p2 <- p.adjust(Permanova_p.2$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+# Permanova_cc.2.q <- bind_cols(Permanova_cc.2,q_cc_p2$qvalue, .name_repair = "minimal")
+# 
+# write.table(Permanova_cc.2.q,"../Analysis/Permanova/PERM_CC_additive.txt" )
+
+
+## PERMDISP Pel -------------------------------------------------------
+# homogeneity of dispersion test between distances
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_P_loc <- permutest(betadisper(aitd_P, samp_p$Location, type = 'median', sqrt.dist = F, add = F ), 
+                       model = 'full', pairwise = F, permutations = perm)
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_P_loc <- p.adjust(PD_P_loc$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_P_loc.q <- bind_cols(PD_P_loc$tab,q_P_loc$qvalue, .name_repair = "minimal") 
+write.table(PD_P_loc.q,"../Analysis/Permdisp/PD_pelagic_loc.txt" )
+
+
+# 4. Calculate distances for CB only --------------------------------------------------------------
+# Aitchison distance it the Euclidean distance of (r)clr transformed data
+aitd_B <- vegdist(clr_i_B, method = "euclidean")
+
+## PERMANOVA CB -------------------------------------------------------
+Permanova_b.1 <- adonis2(aitd_B ~ Location * Backbone * Treatment, data = samp_b, add = T, na.rm = T, permutations = perm )
+Permanova_b.1 
+
+# Permanova_cb.2 <- adonis2(aitd_B ~ Location +  Backbone + Treatment, data = samp_i.cb, add = T, na.rm = T, permutations = perm )
+# Permanova_cb.2 
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_b_p1 <- p.adjust(Permanova_b.1$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+Permanova_b.1.q <- bind_cols(Permanova_b.1,q_b_p1$qvalue, .name_repair = "minimal") 
+
+write.table(Permanova_b.1.q,"../Analysis/Permanova/PERM_benthic.full.txt" )
+
+
+# 
+# q_cb_p2 <- p.adjust(Permanova_cb.2$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+# Permanova_cb.2.q <- bind_cols(Permanova_cb.2,q_cb_p2$qvalue, .name_repair = "minimal")
+# 
+# write.table(Permanova_cb.2.q,"../Analysis/Permanova/PERM_CB_additive.txt" )
+
+## PERMDISP CB -------------------------------------------------------
+# homogeneity of dispersion test between distances
+# The median centroid method is more robust and less sensitive to outliers, aka better for scattered/environmental data
+PD_b_loc <- permutest(betadisper(aitd_B, samp_b$Location, type = 'median', sqrt.dist = F, add = F ), 
+                       model = 'full', pairwise = F, permutations = perm)
+
+# Adjust p-values to q-values, add to df, write table to store results
+q_b_loc <- p.adjust(PD_b_loc$tab$`Pr(>F)`, method = "BH") %>% enframe(value = "qvalue")
+PD_b_loc.q <- bind_cols(PD_b_loc$tab,q_b_loc$qvalue, .name_repair = "minimal") 
+write.table(PD_b_loc.q,"../Analysis/Permdisp/PD_benthic_loc.txt" )
